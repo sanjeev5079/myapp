7f19283209d3c6d3cca5245b9ba7c853c57c80d4
cloudera <cloudera@quickstart.cloudera>
2016-06-07 08:51:51 -0700

    initial commit - myapp

---
+++ myapp/src/main/java/com/gp1/myapp/hello.java
@@ -0,0 +1,12 @@
+package com.gp1.myapp;
+
+public class hello {
+	//comment
+	public static void main(String[] args){
+	System.out.println("Hello myApp");
+	//comment 2
+	System.out.println("Few changes");
+}
+}
+
+
